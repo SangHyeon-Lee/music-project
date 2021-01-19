f695e78136ef8e54da163826717d7894357857b3
euicheon <no1eclee@gmail.com>
2021-01-19 21:03:27 +0900

    first commit to import editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { useState }  from 'react';
+import ReactPlayer from 'react-player';
 import logo from './logo.svg';
 import './App.css';
+import NoteTaking from './note-taking';
+
+
 
 function App() {
+  const [isPaused, setPaused] = useState<Boolean>(false);
+  // const setPausedTrue = () => setPaused(true)
+  const setPausedFalse = () => setPaused(false)
+
+  function noteTaking(): any {
+    setPaused(true)
+    console.log("Note Taking is called");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ReactPlayer
+        className='player'
+        url='videos/Full_Surgeon.mp4'
+        width='80%'
+        height='80%'
+        controls={true}
+        onPause={noteTaking}
+        onPlay={setPausedFalse}
+      />
+      {isPaused && <NoteTaking name='Test' timestamp='0000'
+      />}
     </div>
   );
 }
+
 
 export default App;
---
+++ src/note-taking.tsx
@@ -0,0 +1,33 @@
+import React, {useState, useRef} from 'react';
+import JoditEditor from "jodit-react";
+
+type noteTakingProps = {
+    name: string;
+    timestamp: string;
+};
+
+function NoteTaking({ name, timestamp }: noteTakingProps) {
+
+	const editor = useRef(null)
+	const [content, setContent] = useState('')
+	
+	const config = {
+		readonly: false // all options from https://xdsoft.net/jodit/doc/
+    }
+    
+    return (
+        <>
+            <JoditEditor
+            	ref={editor}
+                value={content}
+                config={config}
+                tabIndex={1} // tabIndex of textarea
+                onBlur={newContent => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
+                onChange={newContent => {}}
+            />
+        </>
+    );
+}
+
+
+export default NoteTaking;
