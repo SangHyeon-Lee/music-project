3113f360502704eb0f02f44e8fad8f1d922f6ce2
Sanghyeon-Lee <lsh1107@kaist.ac.kr>
2021-01-25 17:00:56 +0900

    connect with firebase (01/25)

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,71 @@
-import React, { useState }  from 'react';
-import ReactPlayer from 'react-player';
-import './App.css';
-import NoteTaking from './note-taking';
+import React, { useState } from "react";
+import ReactPlayer from "react-player";
+import "./App.css";
+import NoteTaking from "./note-taking";
+import NoteCollection from "./note-collection";
 
-function App() {
-  const [isPaused, setPaused] = useState<Boolean>(false);
-  //const setPausedTrue = () => setPaused(true)
-  const setPausedFalse = () => setPaused(false)
+class App extends React.Component {
+  state = {
+    duration: 0,
+    secondsElapsed: 0,
+    isPaused: false,
+  };
 
-  function noteTaking(): any {
-    setPaused(true)
+  onDuration = (duration: any) => {
+    this.setState({ duration });
+  };
+  onProgress = (progress: any) => {
+    if (!this.state.duration) {
+      // Sadly we don't have the duration yet so we can't do anything
+      return;
+    }
+
+    // progress.played is the fraction of the video that has been played
+    // so multiply with duration to get number of seconds elapsed
+    const secondsElapsed = Math.floor(progress.played * this.state.duration);
+    console.log(secondsElapsed);
+    if (secondsElapsed !== this.state.secondsElapsed) {
+      this.setState({ secondsElapsed });
+    }
+  };
+
+  noteTaking = () => {
+    const isPaused = true;
+    this.setState({ isPaused });
     console.log("Note Taking is called");
+  };
+
+  setPausedFalse = () => {
+    const isPaused = false;
+    this.setState({ isPaused });
+  };
+
+  render() {
+    return (
+      <div>
+        <ReactPlayer
+          className="player"
+          url="videos/sample.mp4"
+          width="80%"
+          height="80%"
+          controls={true}
+          onPause={this.noteTaking}
+          onPlay={this.setPausedFalse}
+          onDuration={this.onDuration}
+          onProgress={this.onProgress}
+        />
+        <NoteCollection />
+        <div>
+          {this.state.isPaused && (
+            <NoteTaking
+              userId="TestUser"
+              timestamp={this.state.secondsElapsed}
+            />
+          )}
+        </div>
+      </div>
+    );
   }
-
-  return (
-    <div>
-      <ReactPlayer
-        className='player'
-        url='videos/sample.mp4'
-        width='80%'
-        height='80%'
-        controls={true}
-        onPause={noteTaking}
-        onPlay={setPausedFalse}
-      />
-      {isPaused && <NoteTaking name='Test' timestamp='0000'
-      />}
-    </div>
-  );
 }
-
 
 export default App;
--- src/note-taking.tsx
+++ src/note-taking.tsx
@@ -1,38 +1,58 @@
-import React, { useState, useRef } from 'react';
-import { Editor } from '@tinymce/tinymce-react';
+import React, { useState, useRef } from "react";
+import { Editor, EditorState } from "draft-js";
+import "draft-js/dist/Draft.css";
+import "./note-taking.css";
+import { Button } from "antd";
+import { firestore } from "./firebase";
 
-type noteTakingProps = {
-  name: string;
-  timestamp: string;
-};
-
-const NoteTaking = ({ name, timestamp }: noteTakingProps) => {
-
-
-  return (
-
-    <Editor
-      apiKey="ooj1q470wqf1gic73zgpx0jtyqx19mvyj225lgatvd2cwbbh"
-
-      init={{
-        height: 300,
-        placeholder: 'type your notes!',
-        menubar: false,
-        statusbar: false,
-        plugins: [
-          'advlist autolink lists link image charmap print preview anchor',
-          'searchreplace visualblocks code fullscreen save',
-          'insertdatetime media table paste code help wordcount'
-        ],
-        
-        branding: false
-        
-      }}
-      
-    />
-    
-  );
+interface noteTakingProps {
+  userId: string;
+  timestamp: number;
 }
 
+class NoteTaking extends React.Component<noteTakingProps, any> {
+  constructor(props: noteTakingProps) {
+    super(props);
+
+    this.state = { editorState: EditorState.createEmpty() };
+    this.handleChange = this.handleChange.bind(this);
+    this.submitNote = this.submitNote.bind(this);
+  }
+  handleChange(e: EditorState) {
+    this.setState({ editorState: e });
+  }
+  submitNote(): any {
+    const noteCollection = firestore
+      .collection("videos")
+      .doc("testvideo1")
+      .collection("note");
+    noteCollection.doc(this.props.timestamp.toString()).set({
+      category: "Useful",
+      content: this.state.editorState
+        .getCurrentContent()
+        .getPlainText("\u0001"),
+      //timestamp: "",
+      userId: this.props.userId,
+      videoTimestamp: this.props.timestamp,
+    });
+    console.log(
+      this.state.editorState.getCurrentContent().getPlainText("\u0001")
+    );
+  }
+
+  render() {
+    return (
+      <div className="draft-root">
+        <Editor
+          editorState={this.state.editorState}
+          onChange={(e) => this.handleChange(e)}
+        />
+        <Button type="primary" onClick={this.submitNote}>
+          Submit
+        </Button>
+      </div>
+    );
+  }
+}
 
 export default NoteTaking;
