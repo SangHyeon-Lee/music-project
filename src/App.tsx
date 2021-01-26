2b21050a6d0db0579d7fb4fec713ac59cdf3585e
euicheon <no1eclee@gmail.com>
2021-01-26 15:38:03 +0900

    convert App.tsx to functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactPlayer from "react-player";
 import "./App.css";
 import NoteTaking from "./note-taking";
 import NoteCollection from "./note-collection";
+import { Slider } from 'antd';
 
-class App extends React.Component {
-  state = {
-    duration: 0,
-    secondsElapsed: 0,
-    isPaused: false,
-  };
+const marks = {
+  0.5: 'x0.5',
+  1: 'x1',
+  1.5: 'x1.5',
+  2: 'x2',
+  4:'x4'
+};
 
-  onDuration = (duration: any) => {
-    this.setState({ duration });
+interface AppProps {
+  history?: any;
+}
+
+const App: React.FC<AppProps> = props => {
+
+  const [duration,setDuration] = useState<number>(0);
+  const [secondsElapsed,setSecondsElapsed] = useState<number>(0);
+  const [isPaused,setIsPaused] = useState<boolean>(false);
+  const [playbackRate,setPlaybackRate] = useState<number>(1);
+
+
+  const onDuration = (duration: any) => {
+    setDuration(duration)
   };
-  onProgress = (progress: any) => {
-    if (!this.state.duration) {
+  const onProgress = (progress: any) => {
+    if (!duration) {
       // Sadly we don't have the duration yet so we can't do anything
       return;
     }
 
     // progress.played is the fraction of the video that has been played
     // so multiply with duration to get number of seconds elapsed
-    const secondsElapsed = Math.floor(progress.played * this.state.duration);
-    console.log(secondsElapsed);
-    if (secondsElapsed !== this.state.secondsElapsed) {
-      this.setState({ secondsElapsed });
+    const tempsecondsElapsed = Math.floor(progress.played * duration);
+    if (tempsecondsElapsed !== secondsElapsed) {
+      setSecondsElapsed(tempsecondsElapsed)
     }
   };
 
-  noteTaking = () => {
+  const noteTaking = () => {
     const isPaused = true;
-    this.setState({ isPaused });
+    setIsPaused(isPaused)
     console.log("Note Taking is called");
   };
 
-  setPausedFalse = () => {
+  const setPausedFalse = () => {
     const isPaused = false;
-    this.setState({ isPaused });
+    setIsPaused(isPaused)
   };
 
-  render() {
-    return (
+
+  return (
+    <div>
+      <Slider marks={marks} step={null} defaultValue={1} max={4} onChange={(value:any) => setPlaybackRate(value)}/>
+      <ReactPlayer
+        className="player"
+        url="videos/Full_Surgeon.mp4"
+        width="80%"
+        height="80%"
+        controls={true}
+        onPause={noteTaking}
+        onPlay={setPausedFalse}
+        onDuration={onDuration}
+        onProgress={onProgress}
+        playbackRate={playbackRate}
+      />
+      <NoteCollection />
       <div>
-        <ReactPlayer
-          className="player"
-          url="videos/Full_Surgeon.mp4"
-          width="80%"
-          height="80%"
-          controls={true}
-          onPause={this.noteTaking}
-          onPlay={this.setPausedFalse}
-          onDuration={this.onDuration}
-          onProgress={this.onProgress}
-        />
-        <NoteCollection />
-        <div>
-          {this.state.isPaused && (
-            <NoteTaking
-              userId="TestUser"
-              timestamp={this.state.secondsElapsed}
-            />
-          )}
-        </div>
+        {isPaused && (
+          <NoteTaking
+            userId="TestUser"
+            timestamp={secondsElapsed}
+          />
+        )}
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
