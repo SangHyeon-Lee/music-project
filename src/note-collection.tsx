c2a84921f951f90456b9df3c371e0b4e83b58f46
Euicheon <no1eclee@gmail.com>
2021-02-04 17:46:26 +0900

    category-filter updated

--- src/note-collection.tsx
+++ src/note-collection.tsx
@@ -1,31 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import firebase from './firebase'
-import { Button } from 'antd';
+import { Button, Tag } from 'antd';
 import { LikeOutlined } from '@ant-design/icons';
 import './note-collection.css'
+import { LeakAddTwoTone } from "@material-ui/icons";
 
+const { CheckableTag } = Tag;
 var db = firebase.firestore();
 interface noteCollectionProps {}
+const tagsData = ['Awesome', 'What If', 'What & Why', 'Difficult', 'Useful'];
 
-class NoteCollection extends React.Component<noteCollectionProps, any> {
-  constructor(props: noteCollectionProps) {
-    super(props);
+const NoteCollection: React.FC<noteCollectionProps> = (props) => {
+  const [ref, setRef] = useState(db
+                                  .collection("videos")
+                                  .doc("testvideo1")
+                                  .collection("note")
+                                  .orderBy("videoTimestamp"));
+  var unsubscribe = null;
+  const [collection, setCollection] = useState<any[]>([]);
+  const [rightOpen, setRightOpen] = useState(true);
+  const [filter, setFilter] = useState<string[]>([]);
+  const [filteredCollection, setFilteredCollection] = useState<any[]>(collection)
+  const [noteLayout, setNoteLayout] = useState<any[]>();
+  // var originalCollection = null;
 
-    this.ref = db
-      .collection("videos")
-      .doc("testvideo1")
-      .collection("note")
-      .orderBy("videoTimestamp");
-    this.unsubscribe = null;
-    this.state = {
-      collection: [],
-      rightOpen: true,
-    };
-  }
-  ref: any;
-  unsubscribe: any;
-
-  Notecomponent({ note }: any) {
+  useEffect(
+    () => {
+      unsubscribe = ref.onSnapshot(onCollectionUpdate);
+    }, []
+  );
+  
+  const Notecomponent = ({ note }: any) => {
     const videoTime_num: number = note.videoTimestamp;
     const min_val: number = Math.floor(videoTime_num / 60);
     const sec_val: number = videoTime_num % 60;
@@ -49,33 +54,51 @@ class NoteCollection extends React.Component<noteCollectionProps, any> {
     );
   }
 
-  onCollectionUpdate = (querySnapshot: any) => {
+
+
+  const onCollectionUpdate = (querySnapshot: any) => {
     const collection: any = [];
     querySnapshot.forEach((doc: any) => {
       collection.push(doc.data());
     });
-    this.setState({
-      collection,
-    });
+    setCollection(collection)
   };
 
-  componentDidMount() {
-    this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate);
+  const handleChange = (tag:string, checked:boolean) => {
+    const nextSelectedTags = checked ? [...filter, tag] : filter.filter(t => t !== tag);
+    setFilter(nextSelectedTags);
+    let _filteredCollection = collection.filter(item => {
+            if(nextSelectedTags.length > 0 && (nextSelectedTags.indexOf(item.category) > -1)) {
+              return true
+            } else {
+              return false
+            }
+          });
+    // setFilteredCollection(_filteredCollection);
+    return _filteredCollection;
   }
-
-  render() {
-    return (
-      <div>
-        <div className={'collection'}>
-          {this.state.collection.map((note: any, index: any) => (
-            <div>
-            <this.Notecomponent note={note} key={index} />
-            </div>
-          ))}
-        </div>
+  return (
+    <div>
+      <div className={'collection'}>
+        <span style={{ marginRight: 8 }}>Categories:</span>
+              {tagsData.map(tag => (
+                <CheckableTag
+                  key={tag}
+                  checked={filter.indexOf(tag) > -1}
+                  onChange={checked => setFilteredCollection(handleChange(tag, checked))}
+                >
+                  {tag}
+                </CheckableTag>
+              ))}
+        {filteredCollection.map((note: any, index: any) => (
+          <div>
+            <Notecomponent note={note} key={index} />
+          </div>
+        ))}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 
 export default NoteCollection;
