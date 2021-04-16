15367e882d3adf0de671907fd4b7ce11aa103e31
euicheon <no1eclee@gmail.com>
2021-04-16 15:44:45 +0900

    Category Align + Icon for note-editor

--- src/redux/modules/noteCollection.tsx
+++ src/redux/modules/noteCollection.tsx
@@ -25,7 +25,10 @@ export const setCollectionFromDB = (
     .orderBy("videoTimestamp");
   ref.get().then((snap) => {
     snap.forEach((doc) => {
-      if ((doc.data().videoTimestamp < videoDuration) && (category.indexOf(doc.data().category) > -1) && (doc.data().userId===firebase.auth().currentUser?.email?.split("@")[0]! ))
+      if ((doc.data().videoTimestamp < videoDuration) &&
+          (category.indexOf(doc.data().category) > -1) &&
+          ((doc.data().userId===firebase.auth().currentUser?.email?.split("@")[0]! ||
+           firebase.auth().currentUser?.email?.split("@")[0] === "dummy_example" )))
         collection.push(doc.data());
     });
     dispatch(setCollection(collection));
