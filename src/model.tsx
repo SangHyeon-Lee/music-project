b7addbd2428a4031f8d595d8cae94d9317326a44
Sanghyeon Lee <lsh1107@kaist.ac.kr>
2021-08-25 16:39:28 +0900

    add node on model

--- src/model.tsx
+++ src/model.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { memo, useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { useLoader } from "@react-three/fiber";
@@ -10,7 +10,7 @@ import { Suspense } from "react";
 interface ModelProps {}
 
 const Model: React.FC<ModelProps> = ({}) => {
-  
+  const [pos, setPos] = useState([0, 0, 0]);
   const Scene = () => {
     THREE.DefaultLoadingManager.addHandler(/\.dds$/i, new DDSLoader());
     // const materials = useLoader(MTLLoader, "Poimandres.mtl");
@@ -18,12 +18,43 @@ const Model: React.FC<ModelProps> = ({}) => {
       // materials.preload();
     });
 
+    function handleSubmit(e: any) {
+      // e.preventDefault();
+      e.stopPropagation();
+
+      console.log(e);
+      setPos(e.point);
+    }
+
+    function Node(pos: any) {
+      return (
+        <mesh position={pos} receiveShadow castShadow>
+          <sphereBufferGeometry args={[0.1, 16, 16]} />
+          <meshBasicMaterial color={"hotpink"} />
+        </mesh>
+      );
+    }
+
     console.log(obj);
-    return <primitive object={obj} scale={10} />;
+    return (
+      <mesh>
+        <Node pos={pos} />
+        <primitive
+          onPointerOver={(e: any) => e.stopPropagation()}
+          onPointerOut={(e: any) => e.intersections.length === 0}
+          onPointerDown={(e: any) => handleSubmit(e)}
+          object={obj}
+          scale={10}
+        />
+      </mesh>
+    );
   };
   return (
     <div>
-      <Canvas style={{width: "400px", height: "700px", left: "50px"}} camera={{ position: [0, 0, 4] }}>
+      <Canvas
+        style={{ width: "400px", height: "700px", left: "50px" }}
+        camera={{ position: [0, 0, 4] }}
+      >
         <Suspense fallback={null}>
           <Scene />
           <OrbitControls />
